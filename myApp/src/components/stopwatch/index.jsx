85605927c98d2bcf2a89c279745863fe0dd1308d
Utkarsh Gupta <Utkarsh.Gupta@setindia.com>
2025-07-28 21:28:29 +0530

    State

---
+++ myApp/src/components/stopwatch/index.jsx
@@ -0,0 +1,66 @@
+import { useState } from "react";
+import "./stopwatch.css";
+let intervalId;
+const StopWatch = () => {
+  const [time, setTime] = useState(0);
+  const [isPaused, setIsPaused] = useState(false);
+
+  const onStart = () => {
+    setTime((currTime) => currTime + 1);
+    intervalId = setInterval(() => {
+      setTime((currTime) => currTime + 1);
+    }, 1000);
+  };
+  const onPause = () => {
+    clearInterval(intervalId); //stop the interval
+    setIsPaused(true);
+  };
+  const onReset = () => {
+    clearInterval(intervalId); //stop the interval
+    setTime(0);
+  };
+  const onRestart = () => {
+    setIsPaused(false);
+    setTime((currTime) => currTime + 1);
+    intervalId = setInterval(() => {
+      setTime((currTime) => currTime + 1);
+    }, 1000);
+  };
+
+  const isTimerStaterd = time !== 0;
+
+  return (
+    <div className="stop-watch">
+      <div className="stop-watch-time">{time}</div>
+      <div className="stop-watch-actions">
+        {!isTimerStaterd && (
+          <button className="stop-watch-button start-btn" onClick={onStart}>
+            Start
+          </button>
+        )}
+        {isTimerStaterd && (
+          <>
+            {!isPaused && (
+              <button className="stop-watch-button pause-btn" onClick={onPause}>
+                Pause
+              </button>
+            )}
+            {isPaused && (
+              <button
+                className="stop-watch-button restart-btn"
+                onClick={onRestart}
+              >
+                Restart
+              </button>
+            )}
+            <button className="stop-watch-button reset-btn" onClick={onReset}>
+              Reset
+            </button>
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default StopWatch;
--- myApp/src/pages/practice/index.jsx
+++ myApp/src/pages/practice/index.jsx
@@ -1,11 +1,23 @@
+import { useState } from "react";
 import Counter from "../../components/counter";
+import StopWatch from "../../components/stopwatch";
 import TodoList from "../../components/todoList";
 
 const PracticePage = () => {
+  const [showStopWatch, setStopWatch] = useState(false);
   return (
     <div>
       <Counter />
       <TodoList />
+      <div className="stop-watch-wrapper">
+        <button
+          className="stop-watch-toggle-btn"
+          onClick={() => setStopWatch(!showStopWatch)}
+        >
+          {showStopWatch ? "Hide Watch" : "Show Watch"}
+        </button>
+        {showStopWatch && <StopWatch />}
+      </div>
     </div>
   );
 };
